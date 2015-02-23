afa121034faf24bfad65e33a504e59654183be0e
Hisham Muhammad <hisham@gobolinux.org>
2015-02-23 16:42:53 -0300

    Push script updates

--- config.sh
+++ config.sh
@@ -4,4 +4,12 @@ pages_dir=edit_here
 output_dir=generated
 common_dir=common
 package_dir=packed
+pages_data=(generated common)
+extras_data=(images publications)
+
+# server side configuration:
+publish_dir=public_html
+staging_dir="$publish_dir.new"
+server_url=https://github.com/labluapucrio/site/raw/master
+package_url="$server_url/$package_dir"
 
--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 [ -e "LICENSE" ] || { echo "Please run this from the site repository's root directory."; exit 1 ;}
 source ./config.sh
@@ -7,9 +7,9 @@ source ./config.sh
 ./generate.sh
 
 find "$output_dir" | xargs touch -r LICENSE
-tar cv generated common | gzip -n > "$package_dir"/pages.tar.gz 
-tar cv images publications | gzip -n > "$package_dir"/extras.tar.gz 
+tar cv config.sh "${pages_data[@]}" | gzip -n > "$package_dir"/pages.tar.gz 
+tar cv "${extras_data[@]}" | gzip -n > "$package_dir"/extras.tar.gz
 md5sum  "$package_dir"/pages.tar.gz >  "$package_dir"/pages.tar.gz.md5
 md5sum  "$package_dir"/extras.tar.gz >  "$package_dir"/extras.tar.gz.md5
 
-git add "$package_dir"/* && git commit && git push
+git add edit_here "$package_dir"/* && git commit && git push
