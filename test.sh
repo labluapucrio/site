c1e767d82d1c3b7e27993926fcb4c9d4e4779d1b
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2020-08-22 12:33:47 -0300

    Use config.sh in the test script

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 # Use this script to test the changes localy in your browser
 
+[ -e "LICENSE" ] || { echo "Please run this from the site repository's root directory."; exit 1 ;}
+source ./config.sh
+[ "$output_dir" ] || { echo "Couldn't load config.sh"; exit 1 ;}
+
 ./generate.sh running || exit 1
-mkdir -p testdir
-cp -R generated/* common/* images testdir
-cd testdir || exit 1
+mkdir -p "$publish_dir"
+cp -R "$output_dir"/* "$common_dir"/* "${extras_data[@]}" "$publish_dir"
+cd "$publish_dir" || exit 1
 { sleep 1; echo "Opening browser..."; xdg-open 'http://localhost:8000'; } &
 python3 -m http.server
